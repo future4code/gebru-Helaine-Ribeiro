7bdfcae89557d05387ac6efb3130a10e695613ab
Helaine <helainecrcosta@gmail.com>
2021-11-10 22:11:44 -0300

    iniciando o exercicio

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,16 @@
+//1-Leia o código abaixo. Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
+/*1.a.  false, b.  false, c.  true, d.  boolean */ 
