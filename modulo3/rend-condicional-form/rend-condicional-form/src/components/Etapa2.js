d6e38326097550c06719f4a34489dbe4e57cbdd4
Helaine <helainecrcosta@gmail.com>
2022-02-08 21:52:00 -0300

    criando meu primeiro formulário

---
+++ modulo3/rend-condicional-form/rend-condicional-form/src/components/Etapa2.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+class Etapa2 extends React.Component {
+    render(){
+  return (
+     <div className="Etapa2">
+     <h1>INFORMAÇÕES DO ENSINO SUPERIOR</h1>
+     <h3>5. Qual o curso?</h3>
+     <input></input>
+     <h3>6. Qual a unidade de ensino?</h3>
+     <input></input>
+     
+
+    </div>
+  )
+}
+}
+
+export default Etapa2;
---
+++ modulo3/rend-condicional-form/rend-condicional-form/src/components/Etapa3.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+class Etapa3 extends React.Component {
+    render(){
+  return (
+    <div className="Etapa3">
+    <h1>INFORMAÇÕES GERAIS DE ENSINO</h1>
+    <h3>7. Por que você não terminou um curso de graduação?</h3>
+    <input></input>
+    <h3>8. Você fez algum curso complementar?</h3>
+    <input></input>
+    
+
+   </div>
+ )
+}
+}
+
+export default Etapa3;
