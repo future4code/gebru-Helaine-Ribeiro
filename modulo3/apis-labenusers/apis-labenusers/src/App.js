ec1ca1575ae0bfa976f9b865ed8e145c1fe786a4
Helaine Ribeiro <helainecrcosta@gmail.com>
2022-03-08 22:04:17 -0300

    novo

--- modulo3/apis-labenusers/apis-labenusers/src/App.js
+++ modulo3/apis-labenusers/apis-labenusers/src/App.js
@@ -1,12 +1,39 @@
-import react from 'react';
-import './App.css';
+import React from 'react'
+import CadastroUsurio from './components/CadastroUsuario';
+import ListaUsuario from './components/ListaUsuario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> boa noite</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    renderedScreen: 'signUp'
+  }
+
+  chooseScreen = () => {
+    switch (this.state.renderedScreen) {
+      case 'signUp':
+        return <CadastroUsurio goToUsersListScreen={this.goToListaUsuario} />
+      case 'usersList':
+        return <ListaUsuario goToSignUpScreen={this.goToCadastroUsuario} />
+      default:
+        return <h2>Erro! Página não encontrada!</h2>
+    }
+  }
+
+  goToSignUpScreen = () => {
+    this.setState({ renderedScreen: 'signUp' })
+  }
+
+  goToUsersListScreen = () => {
+    this.setState({ renderedScreen: 'usersList' })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>LabenUsers - Gebru</h1>
+        {this.chooseScreen()}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
