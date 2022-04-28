944580af923b2d9e6709711ad334974cc60c5403
Helaine Ribeiro <helainecrcosta@gmail.com>
2022-04-28 19:14:01 -0300

    fazendo estilização da página

--- revisao-pre-react/star-wars-app/src/pages/CharacterListPage/styles.js
+++ revisao-pre-react/star-wars-app/src/pages/CharacterListPage/styles.js
@@ -11,8 +11,6 @@ position: center;
     margin: 350px  0;
     justify-content: space-around;
    
-
-    
    
 `
 
@@ -35,4 +33,32 @@ justify-content: space-around;
     
    background: red; 
 
+`
+const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 500px;
+    margin: 10px 0;
+   
+
+    button{
+        background-color: #455d7a;
+        box-shadow: black 2px 1px 2px 2px;
+    background-color: #455d7a;
+    background-image: linear-gradient(20deg, #455d7a,white);
+    }
+`
+
+ const AdminHomeButtonsContainer= styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center ;
+    width: 500px;
+    border:black 2px solid;
+      box-shadow: black 1px 1px;
+      background-color: #455d7a;
+      padding:20px ;
+      left:10px ;
+      overflow: auto;
 `
